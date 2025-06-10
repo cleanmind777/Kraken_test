8fdd7d8c69beb4d95d8856cf4c0e83ef0ffadd89
cleanmind777 <cleanmind77777@gmail.com>
2025-06-10 17:11:50 +0900

    change: market: del part related env

--- market.py
+++ market.py
@@ -2,11 +2,11 @@ from kraken.spot import Earn, Funding, Market, Trade, User
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
+# load_dotenv()
 
-# Get PUBLIC_KEY and PRIVATE_KEY from .env
-PUBLIC_KEY = os.getenv('PUBLIC_KEY')
-PRIVATE_KEY = os.getenv('PRIVATE_KEY')
+# # Get PUBLIC_KEY and PRIVATE_KEY from .env
+# PUBLIC_KEY = os.getenv('PUBLIC_KEY')
+# PRIVATE_KEY = os.getenv('PRIVATE_KEY')
 
 # # For Fast API
 def get_assets(symbols, public_key, private_key):
